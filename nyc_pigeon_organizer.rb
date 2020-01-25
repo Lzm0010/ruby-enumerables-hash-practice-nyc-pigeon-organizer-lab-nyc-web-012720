a198007a3657260c6fc24feb95809c62fea28238
Lee McCormick <lzm0010@gmail.com>
2020-01-25 16:39:53 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,6 @@
 def nyc_pigeon_organizer(data)
   #Hash of attributes => hashes of attr instances of => array of pigeons with that attribute
-  data
+  data[]
   #get all pigeon names from arrays of hashes and store in an array
   
   #convert array to keys for a new hash 
