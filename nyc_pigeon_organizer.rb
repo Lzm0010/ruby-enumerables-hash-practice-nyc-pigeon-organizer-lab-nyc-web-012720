be835305902d101d8fc19fe4cad52b8cd9672fc7
Lee McCormick <lzm0010@gmail.com>
2020-01-25 17:13:12 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -8,7 +8,7 @@ def nyc_pigeon_organizer(data)
     data[attribute].each_pair{|key, value|
       value.map{|name|
         if pigeon_list[name][attribute]
-          pigeon_list[name] = 
+          pigeon_list[name] = attribute
         else
           pigeon_list[name]
         end
