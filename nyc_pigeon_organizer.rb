b9dac96fa92d4708c3eb1f132ee29b8789796e3b
Lee McCormick <lzm0010@gmail.com>
2020-01-25 16:48:50 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,5 +3,7 @@ def nyc_pigeon_organizer(data)
   attributes = [:color, :gender, :lives]
   
   #get all pigeon names from arrays and convert to a key and the previous key to the value at the same time
-  attributes.each_pair
+  attributes.each{ |attribute|
+    
+  }
 end
